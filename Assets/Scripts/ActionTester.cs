61788f96d208648542c991b2cd6339d1589e93d6
basiakoska <bakos20@student.sdu.dk>
2022-05-19 08:25:00 +0200

    Input control by the use of Zinnie

---
+++ Assets/Scripts/ActionTester.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionTester : MonoBehaviour
+{
+    public void LogMessage(string message)
+    {
+        Debug.Log(message);
+    }
+
+    public void LogFloat(float value)
+    {
+        Debug.Log(value);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zinnia.Action;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody player;
     //public float speed;
-    public float forceMultiplier = 10f; 
-    
+    public float forceMultiplier = 10f;
+    public Vector2Action moveInput;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        //Vector3 moveDelta = moveDirection * speed * Time.deltaTime; //speed * timedelta = distance
+        
+        
+        //Vector3 moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+         //Vector3 moveDelta = moveDirection * speed * Time.deltaTime; //speed * timedelta = distance
         //player.position += moveDelta;
+        Vector3 moveDirection = Vector3.zero;
+        moveDirection += new Vector3(moveInput.Value.x, 0f, moveInput.Value.y);
         Vector3 movingForce = moveDirection * forceMultiplier;
         player.AddForce(movingForce);
     }
